fd64c9660fb9978ff935a7389329f3b5ad592eeb
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-06-12 14:21:30 +0800

    Minor

--- resources/php/connectDB.php
+++ resources/php/connectDB.php
@@ -1,8 +1,8 @@
 <?php
 /* Database connection settings */
 	$servername = "localhost";
-    $username = "muhammad_user1";		//put your phpmyadmin username.(default is "root")
-    $password = "H89EPXkT^^m4";			//if your phpmyadmin has a password put it here.(default is "root")
+    $username = "";		//put your phpmyadmin username.(default is "root")
+    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
     $dbname = "muhammad_oas";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
